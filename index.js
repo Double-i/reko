e84f09a8fd0cd24e568629e78b9582142fff30d5
Ilias <igoujgali@gmail.com>
2022-06-09 13:13:47 +0200

    clean

--- index.js
+++ index.js
@@ -9,12 +9,9 @@ import {
     PutObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { parseUrl } from "@aws-sdk/url-parser";
 import { s3Client, rekoClient } from "./helper.js"; // Helper function that creates an Amazon S3 service client module.
 import { RecognizeCelebritiesCommand } from "@aws-sdk/client-rekognition";
 import { fileURLToPath } from "url";
-import { Hash } from "@aws-sdk/hash-node";
-import { HttpRequest } from "@aws-sdk/protocol-http";
 const upload = multer({ dest: "uploads/" });
 
 const __filename = fileURLToPath(import.meta.url);
@@ -22,7 +19,8 @@ const __dirname = path.dirname(__filename);
 
 const BUCKET_NAME = "workshoprekobucket";
 const REGION = "eu-central-1";
-const pictures = [];
+
+const pictures = []; // db ...
 
 /**
  * Get data from json file
@@ -65,27 +63,12 @@ const uploadFile = (path, name) => {
     );
 };
 
-const run = async () => {
-    try {
-        const data = await s3Client.send(new ListBucketsCommand({}));
-        console.log("Success", data.Buckets);
-        return data; // For unit tests.
-    } catch (err) {
-        console.log("Error", err);
-    }
-};
-
-const barak = "Barack-Obama-Speech-3819398190.jpg";
-const selfie = "selfie.jpg";
-
-const photo = selfie;
-
 /**
  * Call Aws reko to reko...
  * @param {*} photoName
  * @returns
  */
-const recognize_celebrity = async (photoName) => {
+const recognizeCelebrity = async (photoName) => {
     try {
         const response = await rekoClient.send(
             new RecognizeCelebritiesCommand({
@@ -97,46 +80,13 @@ const recognize_celebrity = async (photoName) => {
                 },
             })
         );
-        console.log(response.Labels);
+        console.log("Recognize", response.Labels);
         return response.CelebrityFaces;
     } catch (err) {
         console.log("Error", err);
     }
 };
 
-const app = express();
-const port = 3000;
-
-app.engine(".html", ejs.__express);
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "html");
-
-app.get("/add", (req, res) => {
-    res.render("add");
-});
-
-app.post("/add", upload.single("picture"), async (req, res, next) => {
-    try {
-        console.log("POST /add", pictures);
-
-        const resultat = await uploadFile(req.file.path, req.file.originalname);
-        const celebritiesDetection = await recognize_celebrity(
-            req.file.originalname
-        );
-
-        pictures.push({
-            celebrities: celebritiesDetection,
-            src: req.file.originalname,
-        });
-        console.log("POST /add", pictures);
-        saveToJson(pictures);
-        res.render("add", { status: "0" });
-    } catch (err) {
-        console.error("POST /add", err);
-        res.render("add", { status: "1" });
-    }
-});
 /**
  * Presigned picture => to allow app user to access to s3 image for a certain time
  * @param {*} picture
@@ -154,6 +104,48 @@ const signedPicture = async (picture) => {
     });
     return signedUrl;
 };
+
+const app = express();
+const port = 3000;
+
+app.engine(".html", ejs.__express);
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "html");
+
+/**
+ * GET add picture form endpoint
+ */
+app.get("/add", (req, res) => {
+    res.render("add");
+});
+
+/**
+ * POST add picture endpoint
+ */
+app.post("/add", upload.single("picture"), async (req, res, next) => {
+    try {
+        const resultat = await uploadFile(req.file.path, req.file.originalname);
+        const celebritiesDetection = await recognizeCelebrity(
+            req.file.originalname
+        );
+
+        pictures.push({
+            celebrities: celebritiesDetection,
+            src: req.file.originalname,
+        });
+        saveToJson(pictures);
+        console.log("POST /add succeed");
+        res.render("add", { status: "0" });
+    } catch (err) {
+        console.error("POST /add failed", err);
+        res.render("add", { status: "1" });
+    }
+});
+
+/**
+ * Home endpoint.
+ */
 app.get("/", async (req, res) => {
     let filteredPictures;
     const searchTerms = req.query.search;
@@ -177,11 +169,7 @@ app.get("/", async (req, res) => {
     );
     res.render("home", { pictures: tmpPictures });
 });
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-/**
- * TODO :
- *         - effectuer la recherche
- *
- */
