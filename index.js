afb5516c99bea46351db959c674c33056c701a5f
Ilias <igoujgali@gmail.com>
2022-06-09 13:06:12 +0200

    s3 store + reko + search

--- index.js
+++ index.js
@@ -3,19 +3,57 @@ import multer from "multer";
 import fs from "fs";
 import ejs from "ejs";
 import path from "path";
-import { ListBucketsCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+    GetObjectCommand,
+    ListBucketsCommand,
+    PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { parseUrl } from "@aws-sdk/url-parser";
 import { s3Client, rekoClient } from "./helper.js"; // Helper function that creates an Amazon S3 service client module.
 import { RecognizeCelebritiesCommand } from "@aws-sdk/client-rekognition";
 import { fileURLToPath } from "url";
-import { Console } from "console";
-
+import { Hash } from "@aws-sdk/hash-node";
+import { HttpRequest } from "@aws-sdk/protocol-http";
 const upload = multer({ dest: "uploads/" });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const BUCKET_NAME = "workshoprekobucket";
+const REGION = "eu-central-1";
+const pictures = [];
 
+/**
+ * Get data from json file
+ * @returns
+ */
+const getFromJson = () => {
+    return fs.readFile("db.json", (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        JSON.parse(data).map((pic) => pictures.push(pic));
+    });
+};
+getFromJson();
+/**
+ * Save pictures into a json file
+ * @param {*} object
+ */
+const saveToJson = (object) => {
+    fs.writeFile("db.json", JSON.stringify(object), (error) => {
+        if (error) throw error;
+    });
+};
+
+/**
+ * Upload file to s3
+ * @param {*} path
+ * @param {*} name
+ * @returns
+ */
 const uploadFile = (path, name) => {
     const fileContent = fs.readFileSync(path);
     return s3Client.send(
@@ -42,29 +80,25 @@ const selfie = "selfie.jpg";
 
 const photo = selfie;
 
-const recognize_celebrity = async () => {
+/**
+ * Call Aws reko to reko...
+ * @param {*} photoName
+ * @returns
+ */
+const recognize_celebrity = async (photoName) => {
     try {
         const response = await rekoClient.send(
             new RecognizeCelebritiesCommand({
                 Image: {
                     S3Object: {
                         Bucket: BUCKET_NAME,
-                        Name: photo,
+                        Name: photoName,
                     },
                 },
             })
         );
         console.log(response.Labels);
-        response.CelebrityFaces.forEach((celebrity) => {
-            console.log(`Name: ${celebrity.Name}`);
-            console.log(`ID: ${celebrity.Id}`);
-            console.log(`KnownGender: ${celebrity.KnownGender.Type}`);
-            console.log(`Smile: ${celebrity.Smile}`);
-            console.log("Position: ");
-            console.log(`   Left: ${celebrity.Face.BoundingBox.Height}`);
-            console.log(`  Top : ${celebrity.Face.BoundingBox.Top}`);
-        });
-        return response.length; // For unit tests.
+        return response.CelebrityFaces;
     } catch (err) {
         console.log("Error", err);
     }
@@ -74,83 +108,80 @@ const app = express();
 const port = 3000;
 
 app.engine(".html", ejs.__express);
-
-// Optional since express defaults to CWD/views
 app.set("views", path.join(__dirname, "views"));
-
-// Path to our public directory
-
 app.use(express.static(path.join(__dirname, "public")));
-
-// Without this you would need to
-// supply the extension to res.render()
-// ex: res.render('users.html').
 app.set("view engine", "html");
-
-const pictures = [
-    {
-        src: "./test/test.jpg",
-        celebrities: [{ name: "Brad" }, { name: "Angelina" }],
-    },
-    {
-        src: "./test/test.jpg",
-        celebrities: [{ name: "Brad" }, { name: "Angelina" }],
-    },
-    {
-        src: "./test/test.jpg",
-        celebrities: [{ name: "Brad" }, { name: "Angelina" }],
-    },
-    {
-        src: "./test/test.jpg",
-        celebrities: [{ name: "Brad" }, { name: "Angelina" }],
-    },
-    {
-        src: "./test/test.jpg",
-        celebrities: [{ name: "Brad" }, { name: "Angelina" }],
-    },
-    {
-        src: "./test/test.jpg",
-        celebrities: [{ name: "Brad" }, { name: "Angelina" }],
-    },
-    {
-        src: "./test/test.jpg",
-        celebrities: [{ name: "Brad" }, { name: "Angelina" }],
-    },
-    {
-        src: "./test/test.jpg",
-        celebrities: [{ name: "Brad" }, { name: "Angelina" }],
-    },
-];
 
 app.get("/add", (req, res) => {
     res.render("add");
 });
-app.post("/add", upload.single("picture"), async (req, res, next) => {
-    console.log(req.file);
-    try {
-        console.log("try to upload to s3....");
-        const resultat = await uploadFile(req.file.path, req.file.originalname);
 
-        
-        console.log("S3 response",resultat)
+app.post("/add", upload.single("picture"), async (req, res, next) => {
+    try {
+        console.log("POST /add", pictures);
+
+        const resultat = await uploadFile(req.file.path, req.file.originalname);
+        const celebritiesDetection = await recognize_celebrity(
+            req.file.originalname
+        );
+
+        pictures.push({
+            celebrities: celebritiesDetection,
+            src: req.file.originalname,
+        });
+        console.log("POST /add", pictures);
+        saveToJson(pictures);
         res.render("add", { status: "0" });
     } catch (err) {
-        console.error("Faild to upload to s3", err);
+        console.error("POST /add", err);
         res.render("add", { status: "1" });
     }
 });
-app.get("/", (req, res) => {
-    res.render("home", { pictures });
+/**
+ * Presigned picture => to allow app user to access to s3 image for a certain time
+ * @param {*} picture
+ * @returns
+ */
+const signedPicture = async (picture) => {
+    // Create a command to put the object in the S3 bucket.
+    const command = new GetObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: picture.src,
+    });
+    // Create the presigned URL.
+    const signedUrl = await getSignedUrl(s3Client, command, {
+        expiresIn: 3600,
+    });
+    return signedUrl;
+};
+app.get("/", async (req, res) => {
+    let filteredPictures;
+    const searchTerms = req.query.search;
+    if (searchTerms && searchTerms !== "") {
+        filteredPictures = pictures.filter((picture) => {
+            let match = false;
+            picture.celebrities.map((c) => {
+                if (c.Name.toLowerCase().includes(searchTerms.toLowerCase())) {
+                    match = true;
+                }
+            });
+            return match;
+        });
+    } else {
+        filteredPictures = pictures;
+    }
+    const tmpPictures = await Promise.all(
+        filteredPictures.map(async (picture) => {
+            return { ...picture, src: await signedPicture(picture) };
+        })
+    );
+    res.render("home", { pictures: tmpPictures });
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 /**
  * TODO :
- *         - pouvoir ajouter une image sur express
- *         - pouvoir ajouter une image sur S3
- *         - pouvoir afficher une image stocké sur S3
- *         - effectuer le stockage de reko
  *         - effectuer la recherche
  *
  */
